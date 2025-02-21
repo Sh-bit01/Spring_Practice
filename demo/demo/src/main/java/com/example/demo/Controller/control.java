7bdcc6681f5835de37696f5d5a1f64b3b511bb41
Marlinspike <shreyashgondane099@gmail.com>
2025-02-22 03:45:36 +0530

    daa

--- demo/demo/src/main/java/com/example/demo/Controller/control.java
+++ demo/demo/src/main/java/com/example/demo/Controller/control.java
@@ -1,22 +1,36 @@
 package com.example.demo.Controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
+@RequestMapping("/api")
 public class control {
     @GetMapping
     public String Hello(){
+        Map<String, Object> Response = new HashMap<>();
         return "hii";
     }
 
     @GetMapping("/h")
     public String Hello1(){
+        Map<String, Object> Response = new HashMap<>();
         return "hii12";
     }
 
-    @GetMapping("/error")
-    public String error(){
-        return "error";
+    @GetMapping("/get")
+    public ResponseEntity<Map<String, Object>> getData(HttpServletRequest request){
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", "Success");
+        response.put("clientIp", request.getRemoteAddr());
+        response.put("timestamp", System.currentTimeMillis());
+
+        return ResponseEntity.ok(response);
     }
 }
